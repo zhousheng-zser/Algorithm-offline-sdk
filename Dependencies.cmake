abcdf3211a1564e713da2d9507762048dd580194
zhousheng <1020463364@qq.com>
2022-11-17 09:51:46 +0800

    gx-offline-face-recognition
    Signed-off-by: zhousheng <1020463364@qq.com>

---
+++ Dependencies.cmake
@@ -0,0 +1,29 @@
+include(${CMAKE_CURRENT_LIST_DIR}/third-party/miscellaneous/cmake/GXThirdPartyLibrary.cmake)
+
+if(GX_STATIC_RUNTIME)
+    set(runtime_args CMAKE_ARGS -DGX_STATIC_RUNTIME=ON)
+else()
+    set(runtime_args CMAKE_ARGS -DGX_STATIC_RUNTIME=OFF)
+endif()
+
+if(CMAKE_CROSSCOMPILING)
+    set(additional_args CMAKE_ARGS -DGX_TOOLCHAIN_PREFIX=${GX_TOOLCHAIN_PREFIX})
+    set(toolchain_file ${CMAKE_CURRENT_LIST_DIR}/third-party/miscellaneous/cmake/Armv7Toolchain.cmake)
+else()
+    set(additional_args "")
+    set(toolchain_file "")
+endif()
+
+find_package(Threads REQUIRED)
+
+gx_make_install_third_party_library(
+    GXMiscellaneous
+	REQUIRED
+    PARALLEL_BUILD
+    SYNC_BUILD_TYPE
+    GENERATOR ${CMAKE_GENERATOR}
+    ${runtime_args}
+    ${additional_args}
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/third-party/miscellaneous
+    TOOLCHAIN_FILE ${toolchain_file}
+)
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+
+#include <g6/char8_t_remediation.hpp>
+#include <g6/format_remediation.hpp>
+#include <g6/json_compat.hpp>
+#include <g6/json_extensions.hpp>
+#include <g6/logger.hpp>
+
+namespace glasssix::face {
+    struct baz {
+        GX_BEGIN_FIELDS(baz);
+        GX_FIELD(int, a);
+        GX_FIELD(std::string, b);
+        GX_FIELD(std::vector<int>, c);
+        GX_END_FIELDS;
+
+        GX_JSON_SERIALIZABLE_DEFAULT;
+    };
+
+    void foo() {
+        constexpr auto xstr = U8("123");
+        const char* x       = xstr;
+
+        auto str      = glasssix::format("Hello {}{}", 123, xstr);
+        auto&& logger = glasssix::get_screen_logger();
+
+        logger.line(U8("Hello"));
+        logger.json(glasssix::json(baz{.a{123}, .b{U8("hello")}, .c{1, 2, 3, 4, 5}}));
+        glasssix::json j;
+
+        auto value = j.get<baz>();
+    }
+} // namespace glasssix::face
