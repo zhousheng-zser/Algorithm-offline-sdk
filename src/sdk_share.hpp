e2da9716596a20dc15d3e5c4929a79cc8b47983e
zhousheng <zhousheng@glasssix.com>
2024-01-31 13:55:32 +0800

    rk3588(SDK 2.3.1)  修复天车工bug

--- src/sdk_share.hpp
+++ src/sdk_share.hpp
@@ -17,6 +17,7 @@
 #include "../src/nessus/protocols/playphone.hpp"
 #include "../src/nessus/protocols/posture.hpp"
 #include "../src/nessus/protocols/pump_light.hpp"
+#include "../src/nessus/protocols/pump_vesthelmet.hpp"
 #include "../src/nessus/protocols/refvest.hpp"
 #include "../src/nessus/protocols/romancia.hpp"
 #include "../src/nessus/protocols/selene.hpp"
@@ -135,7 +136,7 @@ namespace glasssix {
             Function["batterypilferers"] = &algo_ptr::set_protocols_handl_batterypilferers;
             Function["fighting"]         = &algo_ptr::set_protocols_handl_fighting;
             Function["flame"]            = &algo_ptr::set_protocols_handl_flame;
-            Function["vesthelmet"]       = &algo_ptr::set_protocols_handl_pump_vesthelmet; //   临时先不加pump_
+            Function["pump_vesthelmet"]  = &algo_ptr::set_protocols_handl_pump_vesthelmet;
             Function["pump_light"]       = &algo_ptr::set_protocols_handl_pump_light;
             Function["smog"]             = &algo_ptr::set_protocols_handl_smog;
             Function["refvest"]          = &algo_ptr::set_protocols_handl_refvest;
@@ -180,8 +181,8 @@ namespace glasssix {
         }
         void set_protocols_handl_pump_vesthelmet() {
             _config->set_pump_vesthelmet(_config->_path);
-            pump_vesthelmet_handle = protocol_ptr.make_instance<pump_vesthelmet>(
-                pump_vesthelmet_new_param{_config->_pump_vesthelmet_config.device, _config->_configure_directory.models_directory});
+            pump_vesthelmet_handle = protocol_ptr.make_instance<pump_vesthelmet>(pump_vesthelmet_new_param{
+                _config->_pump_vesthelmet_config.device, _config->_configure_directory.models_directory});
         }
         void set_protocols_handl_pump_light() {
             _config->set_pump_light(_config->_path);
