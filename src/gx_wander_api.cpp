092ced6508782003af4597815315b1f6a52c3850
zhousheng <zhousheng@glasssix.com>
2024-05-27 14:26:00 +0800

    取消线程池设置权重功能

--- src/gx_wander_api.cpp
+++ src/gx_wander_api.cpp
@@ -118,7 +118,7 @@ namespace glasssix {
                 posture_list_temp.emplace_back(person_list[i]);
         }
         try {
-            auto result_pool = pool->enqueue(0,[&] {
+            auto result_pool = pool->enqueue([&] {
                 std::thread::id id_ = std::this_thread::get_id();
                 if (all_thread_algo_ptr[id_] == nullptr) {
                     all_thread_algo_ptr[id_] = new algo_ptr();
@@ -217,7 +217,7 @@ namespace glasssix {
                 posture_list_temp.emplace_back(person_list[i]);
         }
         try {
-            auto result_pool = pool->enqueue(0,[&] {
+            auto result_pool = pool->enqueue([&] {
                 std::thread::id id_ = std::this_thread::get_id();
                 if (all_thread_algo_ptr[id_] == nullptr) {
                     all_thread_algo_ptr[id_] = new algo_ptr();
@@ -288,7 +288,7 @@ namespace glasssix {
         if (impl_->camera_id == 0)
             return true;
 
-        auto result_pool = pool->enqueue(0,[&] {
+        auto result_pool = pool->enqueue([&] {
             std::thread::id id_ = std::this_thread::get_id();
             if (all_thread_algo_ptr[id_] == nullptr) {
                 all_thread_algo_ptr[id_] = new algo_ptr();
@@ -311,7 +311,7 @@ namespace glasssix {
         if (impl_->camera_id == 0)
             return true;
 
-        auto result_pool = pool->enqueue(0,[&] {
+        auto result_pool = pool->enqueue([&] {
             std::thread::id id_ = std::this_thread::get_id();
             if (all_thread_algo_ptr[id_] == nullptr) {
                 all_thread_algo_ptr[id_] = new algo_ptr();
